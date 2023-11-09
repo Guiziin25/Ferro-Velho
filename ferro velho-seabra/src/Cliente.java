18a8b37ae3be7796b44f448e2c13c787bbbbddf9
Seabroso42 <arthur.seabra@ufrpe.br>
2023-11-09 03:17:21 -0300

    coloquei uns set e uns get
    to ficando menos leigo familia vamo que vamo

--- ferro velho-seabra/src/Cliente.java
+++ ferro velho-seabra/src/Cliente.java
@@ -6,5 +6,46 @@ public class Cliente {
     private String nome;
     private String senha;
     private LocalDate datanasc;
+    public String getCpf(){
+        return this.cpf;
+    }
 
+    public void setCpf(String c) {
+        this.cpf = c;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+    public void setEmail(String e){
+        this.email= e
+    }
+    public String getNome(){
+        return this.nome;
+    }
+    public void setNome(String n){
+        this.nome= n
+    }
+
+    public String getSenha() {
+        return this.senha
+    }
+
+    public void setSenha(String s) {
+        this.senha = s;
+    }
+
+    public LocalDate getDatanasc() {
+        return this.datanasc;
+    }
+    public void setDatanasc(LocalDate n){
+        this.datanasc= n;
+    }
+}
+public Cliente(String c, String e, String n, String s, LocalDate d){
+    this.cpf= c;
+    this.email= e;
+    this.nome= n;
+    this.senha= s;
+    this.datanasc= d;
 }
--- ferro velho-seabra/src/Peca.java
+++ ferro velho-seabra/src/Peca.java
@@ -7,4 +7,17 @@ public class Peca {
     private Tipo;
     private Qualidade;
 
+    public String getNome(){return this.nome}
+    public void setnome(String n){this.nome=n}
+
+    public LocalDate getDataEntrada() {return this.DataEntrada;}
+    public void setDataEntrada(LocalDate data){this.DataEntrada=data}
+
+    public String getDescricao() {return this.Descricao;}
+    public void setDescricao(String desc){this.Descricao=desc}
+}
+public Peca(String n, LocalDate data, String desc){
+    this.Descricao= desc;
+    this.DataEntrada= data;
+    this.nome= n;
 }
